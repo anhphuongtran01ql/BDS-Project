503b0ca240c428df245b805d737b2df43342f3a2
anhphuongtran01ql <anhphuongtran01ql@gmail.com>
2023-04-10 12:15:52 +0700

    Create pagination for post list page

--- bds-project/src/Services/Post/PostServices.js
+++ bds-project/src/Services/Post/PostServices.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 import Global from "../../global";
 
-const fetchAllPosts = async () => {
+const fetchAllPosts = async (paramQuery) => {
   const { data } = await axios.get(
-    `${Global.BASE_API_PATH}/api/v1/post/list`,
+    `${Global.BASE_API_PATH}/api/v1/post/list?page=${
+      paramQuery.page - 1
+    }&size=${paramQuery.pageSize}`
   );
+  // console.log("data services", data);
   return data;
 };
 
