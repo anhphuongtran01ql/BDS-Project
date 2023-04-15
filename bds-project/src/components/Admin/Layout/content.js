dfdd2d103bbd6adcd41abd85f22aafa1010f0717
anhphuongtran01ql <anhphuongtran01ql@gmail.com>
2023-04-15 14:31:25 +0700

    Create layout for display users

--- bds-project/src/components/Admin/Layout/content.js
+++ bds-project/src/components/Admin/Layout/content.js
@@ -10,12 +10,49 @@ import SearchIcon from "@mui/icons-material/Search";
 import { useQuery } from "@tanstack/react-query";
 import { fetchAllUsers } from "../../../Services/User/UserServices";
 
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { BiDetail, BiEdit } from "react-icons/bi";
+import { AiOutlineDelete } from "react-icons/ai";
+import { styled } from "@mui/material/styles";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(even)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
 export default function Content() {
+  const columns = [
+    { id: "username", label: "Username" },
+    { id: "email", label: "Email" },
+    { id: "role", label: "Role" },
+    { label: "Detail" },
+  ];
+
   const { data, isLoading, isFetching, isError } = useQuery({
     queryKey: ["users"],
     queryFn: () => fetchAllUsers(),
   });
-  // console.log("data", data);
+  console.log("data", data);
   if (isLoading) {
     return <>Loading</>;
   }
@@ -36,41 +73,83 @@ export default function Content() {
             >
               <Button variant="contained">Add user</Button>
             </Grid>
-            <Paper sx={{ maxWidth: 1650, margin: "auto", overflow: "hidden" }}>
-              <AppBar
-                position="static"
-                color="default"
-                elevation={0}
-                sx={{ borderBottom: "1px solid rgba(0, 0, 0, 0.12)" }}
-              >
-                <Toolbar>
-                  <Grid container spacing={2} alignItems="center">
-                    <Grid item>
-                      <SearchIcon color="inherit" sx={{ display: "block" }} />
-                    </Grid>
-                    <Grid item xs>
-                      <TextField
-                        fullWidth
-                        placeholder="Search by name..."
-                        InputProps={{
-                          disableUnderline: true,
-                          sx: { fontSize: "default" },
-                        }}
-                        variant="standard"
-                      />
-                    </Grid>
+            <AppBar
+              position="static"
+              color="default"
+              elevation={0}
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                mb: 1,
+                borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
+              }}
+            >
+              <Toolbar>
+                <Grid container spacing={2} alignItems="center">
+                  <Grid item>
+                    <SearchIcon color="inherit" sx={{ display: "block" }} />
                   </Grid>
-                </Toolbar>
-              </AppBar>
+                  <Grid item xs>
+                    <TextField
+                      fullWidth
+                      placeholder="Search by name..."
+                      InputProps={{
+                        disableUnderline: true,
+                        sx: { fontSize: "default" },
+                      }}
+                      variant="standard"
+                    />
+                  </Grid>
+                </Grid>
+              </Toolbar>
+            </AppBar>
+            <Paper sx={{ margin: "auto", overflow: "auto" }}>
               {data ? (
                 <>
-                  <Typography
-                    sx={{ my: 5, mx: 2 }}
-                    color="text.secondary"
-                    align="center"
+                  <TableContainer
+                    sx={{
+                      display: "table",
+                      tableLayout: "fixed",
+                    }}
                   >
-                    Data
-                  </Typography>
+                    <Table style={{ minWidth: 600 }}>
+                      <TableHead>
+                        <TableRow>
+                          {columns.map((column) => (
+                            <StyledTableCell
+                              key={column.id}
+                              align={column.align}
+                              style={{ minWidth: column.minWidth }}
+                            >
+                              {column.label}
+                            </StyledTableCell>
+                          ))}
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {data.map((list, index) => (
+                          <StyledTableRow key={list.username}>
+                            <StyledTableCell component="th" scope="row">
+                              {list.username}
+                            </StyledTableCell>
+                            <StyledTableCell>{list.email}</StyledTableCell>
+                            <StyledTableCell>
+                              {list.roleList[0].roleName}
+                            </StyledTableCell>
+                            <StyledTableCell>
+                              <BiDetail size={18} style={{ marginRight: 5 }} />
+                              <BiEdit size={18} style={{ marginRight: 5 }} />
+                              <AiOutlineDelete
+                                size={18}
+                                style={{ marginRight: 5 }}
+                                color="red"
+                              />
+                            </StyledTableCell>
+                          </StyledTableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
                 </>
               ) : (
                 <Typography
@@ -78,7 +157,7 @@ export default function Content() {
                   color="text.secondary"
                   align="center"
                 >
-                  No users for this project yet
+                  No users for this system yet
                 </Typography>
               )}
             </Paper>
